406130483761940bb6f2281689f594db187b5aa6
radije <radije@gmail.com>
2013-05-13 16:27:14 +0200

    Añadida la parte básica de fichas y dados.

--- src/cat/montoya/gbd/GameDetail.java
+++ src/cat/montoya/gbd/GameDetail.java
@@ -3,13 +3,29 @@ package cat.montoya.gbd;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class GameDetail extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_game_detail);
+		
+		Spinner spinner = (Spinner) findViewById(R.id.numer_dices);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.number_dices_array, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adapter);
+		
+		
+		Spinner spinnerChips = (Spinner) findViewById(R.id.numer_chips);
+		spinnerChips.setAdapter(adapter);
 	}
 
 	@Override
