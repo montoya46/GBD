d4609e663c36dd51de96216fb640d9ccef9a4366
radije <radije@gmail.com>
2013-08-24 11:43:37 +0200

    Avanzado el diseño del color picker

--- src/cat/montoya/gbd/DialogColorPicker.java
+++ src/cat/montoya/gbd/DialogColorPicker.java
@@ -1,29 +1,51 @@
 package cat.montoya.gbd;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cat.montoya.gbd.adapters.ColorAdapter;
+import cat.montoya.gbd.adapters.ImageAdapter;
+
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.GridView;
 
 public class DialogColorPicker extends DialogFragment {
-	private EditText mEditText;
 
+	private List<Integer> colores;
+	
     public DialogColorPicker() {
         // Empty constructor required for DialogFragment
+    	colores = new ArrayList<Integer>();
+    	colores.add(Color.BLACK);
+    	colores.add(Color.DKGRAY);
+    	colores.add(Color.GRAY);
+    	colores.add(Color.LTGRAY);
+    	colores.add(Color.BLUE);
+    	colores.add(Color.CYAN);
+    	colores.add(Color.GREEN);
+    	colores.add(Color.RED);
+    	colores.add(Color.MAGENTA);
+    	colores.add(Color.YELLOW);
+    	
     }
 
 //    @Override
 //    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 //    	
 //        View view = inflater.inflate(R.layout.dialog_color_picker, container);
-//        mEditText = (EditText) view.findViewById(R.id.txt_your_name);
-//        getDialog().setTitle("Hello");
+//        GridView gvColores = (GridView) view.findViewById(R.id.gvColores);
 //
 //        return view;
 //    }
@@ -36,32 +58,18 @@ public class DialogColorPicker extends DialogFragment {
     
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-//        return new AlertDialog.Builder(getActivity())
-//                .setPositiveButton("OK",
-//                    new DialogInterface.OnClickListener() {
-//                        public void onClick(DialogInterface dialog, int whichButton) {
-//                            
-//                        }
-//                    }
-//                )
-//                .setNegativeButton("Cancel",
-//                    new DialogInterface.OnClickListener() {
-//                        public void onClick(DialogInterface dialog, int whichButton) {
-//                            
-//                        }
-//                    }
-//                )
-//                .create();
-        
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        builder.setTitle(R.string.TituloSeleccionaColor);
+        
+        builder.setPositiveButton(R.string.Aceptar, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
                 
             }
     	});
         
-        builder.setNegativeButton("Cancel",
+        builder.setNegativeButton(R.string.Cancelar,
         	    new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
                 
@@ -69,6 +77,8 @@ public class DialogColorPicker extends DialogFragment {
         });
 
         View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_color_picker, null);
+        GridView gvColores = (GridView) view.findViewById(R.id.gvDialogColores);
+        gvColores.setAdapter(new ColorAdapter(getActivity(), colores));
         builder.setView(view);
 
         return builder.create();
---
+++ src/cat/montoya/gbd/adapters/ColorAdapter.java
@@ -0,0 +1,56 @@
+package cat.montoya.gbd.adapters;
+
+import java.util.List;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.RelativeLayout;
+import cat.montoya.gbd.R;
+
+public class ColorAdapter extends BaseAdapter {
+	private List<Integer> colors;
+	private static LayoutInflater inflater = null;
+
+	public ColorAdapter(Activity activity, List<Integer> colors) {
+		this.colors = colors;
+
+		if (ColorAdapter.inflater == null) {
+			ColorAdapter.inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		}
+
+	}
+
+	public int getCount() {
+		return colors.size();
+	}
+
+	public Integer getItem(int position) {
+		if (position < colors.size()) {
+			return colors.get(position);
+		} else {
+			return colors.get(colors.size() - 1);
+		}
+	}
+
+	public long getItemId(int position) {
+		return 0;
+	}
+
+	// create a new ImageView for each item referenced by the Adapter
+	public View getView(int position, View convertView, ViewGroup parent) {
+		int color = getItem(position);
+
+		View vi = convertView;
+		if (convertView == null)
+			vi = inflater.inflate(R.layout.dialog_color_item, null);
+
+		RelativeLayout rlColor = (RelativeLayout) vi.findViewById(R.id.rlDialogColorItem);
+		
+		rlColor.setBackgroundColor(color);
+				
+		return vi;
+	}
+}
